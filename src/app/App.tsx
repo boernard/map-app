c2f1f2cff77c676aecf7e65e8b980b71fe2f4ea2
boernard <boernard@gmail.com>
2020-04-15 23:42:02 +0200

    refactors how map state is updated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './static/normalize.css'
+import './static/style.css'
 import { MapAppController } from './components/App';
 declare let module: any
 
--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -38,18 +38,12 @@ const mockData: MapItems[] = [
 
 const MapAppController = () => {
   const [mapCenter, setMapCenter] = useState([9.96403, 53.56826] as MapLocation)
-  const [readMapCenter, setReadCenter] = useState([9.96403, 53.56826] as MapLocation)
-
-  useEffect(() => {
-    if (!_.isEqual(readMapCenter, mapCenter)) {
-      setMapCenter(readMapCenter)
-    }
-  }, [readMapCenter])
+  const [readMapCenter, setReadMapCenter] = useState([9.96403, 53.56826] as MapLocation)
 
   return (
     <>
       <div className='mapView'>
-        <MapView center={mapCenter} readCenter={readMapCenter} setReadCenter={setReadCenter} locations={mockData} />
+        <MapView mapCenter={mapCenter} setMapCenter={setMapCenter} readMapCenter={readMapCenter} setReadMapCenter={setReadMapCenter} locations={mockData} />
       </div>
       <div className='contentView'>
         <ContentView setMapCenter={setMapCenter} locations={mockData} />
@@ -71,7 +65,7 @@ const ContentView = (props) => {
       {locations.map(l => {
         return (
           <div key={l.id}>
-            <h2><a href='x' onClick={(ev) => handleClick(ev, l.coordinates)}>{l.title}</a></h2>
+            <h3><a href='x' onClick={(ev) => handleClick(ev, l.coordinates)}>{l.title}</a></h3>
             {l.description}
           </div>
         )
@@ -84,22 +78,32 @@ const Map = ReactMapboxGl({
   accessToken: process.env.MAPBOX_KEY
 })
 
-const MapView = (props: any) => {
-  const center = props.center
-  const setReadCenter = props.setReadCenter
+const MapView = React.memo((props: any) => {
+  const mapCenter = props.mapCenter
+  const setMapCenter = props.setMapCenter
+  const setReadMapCenter = props.setReadMapCenter
+  const readMapCenter = props.readMapCenter
   const locations = props.locations
   const [zoom, setZoom] = useState([12] as [number])
 
-  const handleMoveEnd = useCallback(_.debounce((map: mapboxgl.Map) => {
-    console.log("zoooom")
-    setReadCenter(map.getCenter().toArray().map(i => Number(i.toFixed(5))))
+  const equalizedMapCenter = [...mapCenter].map(i => Number(i.toFixed(5)))
+
+  useEffect(() => {
+    if (!_.isEqual(equalizedMapCenter, readMapCenter)) {
+      setMapCenter(readMapCenter)
+    }
+  }, [readMapCenter])
+
+  const handleMoveEnd = (map: mapboxgl.Map) => {
+    console.log("handleMoveEnd")
+    setReadMapCenter(map.getCenter().toArray().map(i => Number(i.toFixed(5))))
     setZoom([map.getZoom()])
-  }, 500), [])
+  }
 
   return (
     <Map
       style="mapbox://styles/mapbox/streets-v9"
-      center={center}
+      center={mapCenter}
       zoom={zoom}
       movingMethod="easeTo"
       onMoveEnd={map => handleMoveEnd(map)}
@@ -115,6 +119,6 @@ const MapView = (props: any) => {
       </Layer>
     </Map>
   )
-}
+})
 
 export { MapView, ContentView, MapAppController }
