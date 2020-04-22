d22761473589cce48117fee815ad419c00cf616b
boernard <boernard@gmail.com>
2020-04-22 23:08:02 +0200

    implement production build script

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const path = require('path'),
-    express = require('express'),
-    webpack = require('webpack'),
-    webpackConfig = require('./webpack.config.js'),
-    app = express(),
-    port = process.env.PORT || 3000;
+  express = require('express'),
+  webpack = require('webpack'),
+  app = express(),
+  port = process.env.PORT || 3000
+let webpackConfigPath
 
-app.listen(port, () => { console.log(`App is listening on port ${port}`) });
+if (process.env.NODE_ENV !== 'production') {
+  webpackConfigPath = './webpack.prod.js'
+} else {
+  webpackConfigPath = './webpack.dev.js'
+}
+const webpackConfig = require(webpackConfigPath)
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`)
+})
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-});
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
 
-let compiler = webpack(webpackConfig);
-app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true, publicPath: webpackConfig.output.publicPath, stats: { colors: true }
-}));
-app.use(require('webpack-hot-middleware')(compiler));
-app.use(express.static(path.resolve(__dirname, 'dist')));
+let compiler = webpack(webpackConfig)
+app.use(
+  require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+    stats: { colors: true },
+  })
+)
+app.use(require('webpack-hot-middleware')(compiler))
+app.use(express.static(path.resolve(__dirname, 'dist')))
---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path'),
+  webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+  Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'app', 'index.html'),
+      title: 'Production',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  entry: {
+    app: ['./src/app/App.tsx', 'webpack-hot-middleware/client'],
+    vendor: ['react', 'react-dom'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+}
